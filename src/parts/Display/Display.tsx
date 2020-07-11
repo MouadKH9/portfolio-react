227daa5ad692bf2e06d13f8e36b49dec1ac57d42
mouadkh <mouad.khchich@gmail.com>
2020-07-11 12:12:27 +0100

    Better display

--- src/parts/Display/Display.tsx
+++ src/parts/Display/Display.tsx
@@ -1,45 +1,42 @@
 import React from "react";
 import styled from "styled-components";
-import Divider from "../../components/Divider/Divider";
 
 export default function Display() {
-    return (
-        <DisplayContainer className="animate-bg">
-            <TextContainer>
-                <Title>Mouad K.</Title>
-                <Divider theme={{ fg: "white" }} />
-                <Subtitle>
-                    Full Stack Web Developer - Software Programmer
-                </Subtitle>
-            </TextContainer>
-        </DisplayContainer>
-    );
+	return (
+		<DisplayContainer className="animate-bg">
+			<TextContainer>
+				<Title>Mouad K.</Title>
+				<Subtitle>Full Stack Web Developer - Software Engineer</Subtitle>
+			</TextContainer>
+		</DisplayContainer>
+	);
 }
 
 const DisplayContainer = styled.div`
-    width: 100%;
-    height: 100vh;
+	width: 100%;
+	height: 100vh;
 `;
 
 const TextContainer = styled.div`
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    background-color: rgba(255, 255, 255, 0.1);
-    padding: 10px;
-    border-radius: 20px;
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	background-color: rgba(255, 255, 255, 0.1);
+	padding: 10px;
+	border-radius: 20px;
 `;
 
 const Title = styled.h1`
-    color: white;
-    font-size: 4.5em;
-    font-weight: 800;
-    text-align: center;
-    text-transform: uppercase;
+	color: white;
+	font-size: 4.5em;
+	font-weight: 800;
+	text-align: center;
+	text-transform: uppercase;
 `;
 
 const Subtitle = styled.h3`
-    text-align: center;
-    color: white;
+	text-align: center;
+	color: white;
+	margin-top: 20px;
 `;
