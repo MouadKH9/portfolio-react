eed1fc180f89ba353ff4cd817fd805dfd740f2a6
mouadkh <mouad.khchich@gmail.com>
2020-07-22 12:35:21 +0100

    Added admin routing

--- src/AdminLayout.tsx
+++ src/AdminLayout.tsx
@@ -1,34 +1,21 @@
 import React from "react";
 import AdminCard from "./components/Admin/AdminCard/AdminCard";
-import { useFirestoreCollectionData, useFirestore } from "reactfire";
-import { ProjectInterface } from "./parts/Portfolio/types";
-import ProjectItem from "./components/Admin/Project/ProjectItem";
-import Button from "react-bootstrap/esm/Button";
-import Row from "react-bootstrap/esm/Row";
-import Col from "react-bootstrap/esm/Col";
-import Container from "react-bootstrap/esm/Container";
+import Projects from "./parts/Admin/Projects/Projects";
+import { Switch, Route, Redirect } from "react-router-dom";
+import AddProject from "./parts/Admin/AddProject/AddProject";
 
 export default function AdminLayout() {
-	const projectsRef = useFirestore().collection("projects");
-	const projectDocs = useFirestoreCollectionData<ProjectInterface>(projectsRef);
 	return (
 		<AdminCard title="Projects Management">
-			<Container>
-				<Row>
-					<Col sm={12} md={{ span: 2, offset: 10 }}>
-						<Button className="mb-3 w-100">
-							<i className="fas fa-plus-circle mr-1"></i>
-							Add a project
-						</Button>
-					</Col>
-				</Row>
-
-				{projectDocs.map((project: ProjectInterface, index: number) => (
-					<div>
-						<ProjectItem project={project} />
-					</div>
-				))}
-			</Container>
+			<Switch>
+				<Route path="/admin/projects" component={Projects} />
+				<Route path="/admin/add-project" component={AddProject} />
+				<Route
+					path="*"
+					exact={true}
+					component={() => <Redirect to="/admin/projects" />}
+				/>
+			</Switch>
 		</AdminCard>
 	);
 }
---
+++ src/parts/Admin/Projects/Projects.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { useFirestoreCollectionData, useFirestore } from "reactfire";
+
+import ProjectItem from "../../../components/Admin/Project/ProjectItem";
+import { ProjectInterface } from "../../Portfolio/types";
+
+import Button from "react-bootstrap/esm/Button";
+import Row from "react-bootstrap/esm/Row";
+import Col from "react-bootstrap/esm/Col";
+import Container from "react-bootstrap/esm/Container";
+import { Link } from "react-router-dom";
+
+export default function Projects() {
+	const projectsRef = useFirestore().collection("projects");
+	const projectDocs = useFirestoreCollectionData<ProjectInterface>(projectsRef);
+	return (
+		<Container>
+			<Row>
+				<Col sm={12} md={{ span: 2, offset: 10 }}>
+					<Link to="/admin/add-project">
+						<Button className="mb-3 w-100">
+							<i className="fas fa-plus-circle mr-1"></i>
+							Add a project
+						</Button>
+					</Link>
+				</Col>
+			</Row>
+
+			{projectDocs.map((project: ProjectInterface, index: number) => (
+				<div>
+					<ProjectItem project={project} />
+				</div>
+			))}
+		</Container>
+	);
+}
