14b332f4a881dcc075cff86f8d2a17822a1b4151
mouadkh <mouad.khchich@gmail.com>
2020-07-23 17:43:54 +0100

    Started on add project

--- src/parts/Admin/AddProject/AddProject.tsx
+++ src/parts/Admin/AddProject/AddProject.tsx
@@ -7,6 +7,8 @@ import { EditorState, convertToRaw } from "draft-js";
 import { Editor } from "react-draft-wysiwyg";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import draftToHtml from "draftjs-to-html";
+import Row from "react-bootstrap/esm/Row";
+import Col from "react-bootstrap/esm/Col";
 
 export default function AddProject() {
 	const [editorState, setEditorState] = useState(EditorState.createEmpty());
@@ -18,26 +20,50 @@ export default function AddProject() {
 	return (
 		<Container>
 			<Form>
-				<Form.Group controlId="title">
-					<Form.Label>Title</Form.Label>
-					<Form.Control type="text" placeholder="Title" />
-				</Form.Group>
-				<Form.Group controlId="description">
-					<Form.Label>Description</Form.Label>
-					<Editor
-						editorState={editorState}
-						onEditorStateChange={(state) => setEditorState(state)}
-					/>
-				</Form.Group>
-
-				<Form.Group controlId="title">
-					<Form.Label>Title</Form.Label>
-					<Form.Control type="text" placeholder="Title" />
-				</Form.Group>
-
-				<Button variant="primary" type="submit" onClick={addProject}>
-					Add
-				</Button>
+				<Row>
+					<Col sm={12}>
+						<Form.Group controlId="title">
+							<Form.Label>Title</Form.Label>
+							<Form.Control type="text" placeholder="Title" />
+						</Form.Group>
+					</Col>
+					<Col sm={12}>
+						<Form.Group controlId="description">
+							<Form.Label>Description</Form.Label>
+							<Editor
+								editorState={editorState}
+								onEditorStateChange={(state) => setEditorState(state)}
+							/>
+						</Form.Group>
+					</Col>
+					<Col sm={12} md={6}>
+						<Form.Group controlId="tags">
+							<Form.Label>Tags</Form.Label>
+							<Form.Control
+								type="text"
+								placeholder="Separate your tags with a comma"
+							/>
+						</Form.Group>
+					</Col>
+					<Col sm={12} md={6}>
+						<Form.Group controlId="link">
+							<Form.Label>Link</Form.Label>
+							<Form.Control type="text" placeholder="Link" />
+						</Form.Group>
+					</Col>
+				</Row>
+				<Row>
+					<Col sm={12} md={{ span: 6, offset: 3 }}>
+						<Button
+							variant="primary"
+							type="submit"
+							className="w-100 my-2"
+							onClick={addProject}
+						>
+							Add
+						</Button>
+					</Col>
+				</Row>
 			</Form>
 		</Container>
 	);
