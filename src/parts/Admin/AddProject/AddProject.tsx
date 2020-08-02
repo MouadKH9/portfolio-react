b9c72cb0de8a9e99c638a2b499aed602969eaaa5
mouadkh <mouad.khchich@gmail.com>
2020-08-02 11:49:32 +0100

    Refactored tags

--- src/parts/Admin/AddProject/AddProject.tsx
+++ src/parts/Admin/AddProject/AddProject.tsx
@@ -10,7 +10,6 @@ import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import draftToHtml from "draftjs-to-html";
 import Row from "react-bootstrap/esm/Row";
 import Col from "react-bootstrap/esm/Col";
-import colors from "../../../utils/colors";
 import styled from "styled-components";
 import { ProjectInterface } from "../../Portfolio/types";
 import { format } from "date-fns";
@@ -20,6 +19,7 @@ import {
 	useStorage,
 } from "reactfire";
 import { shiftProjects } from "../../../utils/firebaseUtills";
+import Tags from "./Tags";
 
 export default function AddProject() {
 	const history = useHistory();
@@ -32,7 +32,6 @@ export default function AddProject() {
 
 	const [title, setTitle] = useState("");
 	const [tags, setTags] = useState<string[]>([]);
-	const [tagsText, setTagsText] = useState("");
 	const [link, setLink] = useState("");
 	const [atStart, setAtStart] = useState(false);
 	const [editorState, setEditorState] = useState(EditorState.createEmpty());
@@ -40,17 +39,14 @@ export default function AddProject() {
 	const [loading, setLoading] = useState(false);
 	const [progress, setProgress] = useState(0);
 
-	const tagsKeyUp = (ev: React.KeyboardEvent) => {
-		if (ev.key !== ",") return;
-
-		setTags([...tags, tagsText.substring(0, tagsText.length - 1)]);
-		setTagsText("");
-	};
-
 	const removeTag = (index: number) => () => {
 		const clonedTags = Array.from(tags);
 		clonedTags.splice(index, 1);
 		setTags(clonedTags);
+	};
+
+	const addTag = (tag: string) => () => {
+		setTags([...tags, tag]);
 	};
 
 	const handleImageAsFile = (e: any) => {
@@ -142,22 +138,7 @@ export default function AddProject() {
 					</Col>
 					<Col sm={12} md={6}>
 						<Form.Group controlId="tags">
-							<Form.Label>Tags</Form.Label>
-							<Form.Control
-								type="text"
-								placeholder="Separate your tags with a comma"
-								value={tagsText}
-								onChange={(ev: any) => setTagsText(ev.target.value)}
-								onKeyUp={tagsKeyUp}
-							/>
-							<TagContainer>
-								{tags.map((tag, index) => (
-									<Tag key={index} onClick={removeTag(index)}>
-										{tag}
-									</Tag>
-								))}
-								{!tags.length && <Info>Your tags will appear here..</Info>}
-							</TagContainer>
+							<Tags tags={tags} addTag={addTag} removeTag={removeTag} />
 						</Form.Group>
 					</Col>
 					<Col sm={12} md={6}>
@@ -207,22 +188,4 @@ export default function AddProject() {
 
 const TagContainer = styled.div`
 	padding-top: 17px;
-`;
-
-const Tag = styled.span`
-	background-color: ${colors.dark};
-	padding: 5px;
-	color: white;
-	border-radius: 8px;
-	margin-right: 5px;
-	cursor: pointer;
-	transition: all 0.3s;
-
-	&:hover {
-		background-color: ${colors.lighterDark};
-	}
-`;
-
-const Info = styled.span`
-	color: lightgray;
 `;
